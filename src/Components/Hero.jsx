0f608227fe0e1bfbf48539f68af87530e7e75473
DESKTOP-2UVV9SF\Dream Monster <sulemanshakeel37@gmail.com>
2024-05-11 09:50:03 -0700

    adding motion in the code

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,192L80,208C160,224,320,256,480,229.3C640,203,800,117,960,101.3C1120,85,1280,139,1360,165.3L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
       </div>
       {/* SPECIALIZATION */}
-      <div className="container-fluid specialization" id="about">
+      <div className="container-fluid specialization text-center" id="about">
         <div className="container">
           <div className="row">
             <div className="col  text-center mt-3   animate__animated animate__jackInTheBox" loading="lazy">
